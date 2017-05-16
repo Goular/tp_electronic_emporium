9b7a1a58eab63628f8b71a82dea3c538c1ed248a
Goular <zhaojt_cs@126.com>
2017-05-16 10:56:58 +0800

    添加内容

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //前台管理页面的CSS访问路径
+    'HOME_CSS_URL' => __ROOT__ . '/Public/Home/Styles',
+    //前台管理页面的Js访问路径
+    'HOME_JS_URL' => __ROOT__ . '/Public/Home/Js',
+    //前台管理页面的Images访问路径
+    'HOME_IMG_URL' => __ROOT__ . '/Public/Home/Images'
 );
