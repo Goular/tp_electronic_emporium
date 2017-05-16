90a9a26dd9cb2c929f35aaf3d803c11689d1e3ad
Goular <zhaojt_cs@126.com>
2017-05-16 10:21:09 +0800

    提交前端的资源文件

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 后台首页控制器
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function top()
+    {
+        $this->display();
+    }
+
+    public function menu()
+    {
+        $this->display();
+    }
+
+    public function main()
+    {
+        $this->display();
+    }
+}
