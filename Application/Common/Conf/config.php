7d7e89d5b3451e3253c23ec61eaa80d94519d21a
Goular <zhaojt_cs@126.com>
2017-05-18 19:45:06 +0800

    添加I方法的过滤规则

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,4 +10,5 @@ return array(
     'DB_PREFIX' => 'p39_',    // 数据库表前缀
     'DB_CHARSET' => 'utf8',      // 数据库编码
     'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志
+    'DEFAULT_FILTER'         => 'htmlspecialchars,trim', // 默认参数过滤方法 用于I函数...
 );
