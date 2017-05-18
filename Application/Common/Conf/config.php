11317be1eeb30736d4e4cc12659af8a91643cacc
Goular <zhaojt_cs@126.com>
2017-05-18 11:12:50 +0800

    变更添加字段

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //PDO连接方式连接数据库
+    'DB_TYPE'   => 'pdo', // 数据库类型
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '123456', // 密码
+    'DB_PREFIX' => 'p39_', // 数据库表前缀
+    'DB_DSN'    => 'mysql:host=localhost;dbname=php39;charset=utf8'
 );
