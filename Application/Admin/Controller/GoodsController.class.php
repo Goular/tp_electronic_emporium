23858d9754751b623067d524c04aae8cc3ede35a
Goular <zhaojt_cs@126.com>
2017-05-18 11:07:25 +0800

    变更添加字段

--- Application/Admin/Controller/GoodsController.class.php
+++ Application/Admin/Controller/GoodsController.class.php
@@ -7,10 +7,35 @@ use Think\Controller;
 class GoodsController extends Controller
 {
     /**
-     * 添加商品
+     * 添加商品,显示并添加处理
      */
     public function add()
     {
+        //判断是否提交了表单
+        if (IS_POST) {
+            $model = M('goods');
+            /**
+             * CREATE方法：a. 接收数据并保存到模型中 b.根据模型中定义的规则验证表单
+             *
+             * 第一个参数：要接收的数据默认是$_POST
+             * 第二个参数：表单的类型。当前是添加还是修改的表单,1：添加 2：修改
+             * $_POST：表单中原始的数据 ，I('post.')：过滤之后的$_POST的数据，过滤XSS攻击
+             */
+            if ($model->create(I('post.'), 1)) {
+                //创建并校验后添加到数据库中
+                // 在add()里又先调用了_before_insert方法,用于添加addTime字段
+                if($model->add()){ //添加成功后，转跳到页面并结束后面的操作
+                    $this->success('操作成功!',U('lst')); //U方法为创建URL的访问内容
+                    exit();
+                }
+                // 如果走到 这说明上面失败了在这里处理失败的请求
+                // 从模型中取出失败的原因
+                $error = $model->getError();
+                // 由控制器显示错误信息,并在3秒跳回上一个页面
+                $this->error($error);
+            }
+        }
+        //显示表单
         $this->display();
     }
 
