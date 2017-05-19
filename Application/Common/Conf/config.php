11584eefdde3bc6c639383328f2ce99c85937c84
Goular <zhaojt_cs@126.com>
2017-05-19 17:25:16 +0800

    提交商品的内容

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,7 +9,8 @@ return array(
     'DB_PORT' => '3306',        // 端口
     'DB_PREFIX' => 'p39_',    // 数据库表前缀
     'DB_CHARSET' => 'utf8',      // 数据库编码
-    'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志,
+    'SHOW_PAGE_TRACE' => true,
     'DEFAULT_FILTER' => 'trim,htmlspecialchars', // 默认参数过滤方法 用于I函数...
     'UMEDITOR_URL' => __ROOT__ . '/Public/Tools/umeditor1.2.3-utf8-php'
 );
