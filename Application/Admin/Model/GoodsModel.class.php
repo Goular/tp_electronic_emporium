2236c7b47d28a4130fb51e708910b09de5a1f688
Goular <zhaojt_cs@126.com>
2017-05-26 14:25:42 +0800

    修改添加图片你的逻辑代码

--- Application/Admin/Model/GoodsModel.class.php
+++ Application/Admin/Model/GoodsModel.class.php
@@ -25,46 +25,23 @@ class GoodsModel extends Model
     {
         /**************** 处理LOGO *******************/
         // 判断有没有选择图片
-        if ($_FILES['logo']['error'] == 0) {
-            $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize = 1024 * 1024; // 1M
-            $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->rootPath = './Public/Uploads/'; // 设置附件上传根目录
-            $upload->savePath = 'Goods/'; // 设置附件上传（子）目录
-            // 上传文件
-            $info = $upload->upload();
-            if (!$info) {
-                // 获取失败原因把错误信息保存到 模型的error属性中，然后在控制器里会调用$model->getError()获取到错误信息并由控制器打印
-                $this->error = $upload->getError();
-                return FALSE;
-            } else {
-                /**************** 生成缩略图 *****************/
-                // 先拼成原图上的路径
-                $logo = $info['logo']['savepath'] . $info['logo']['savename'];
-                // 拼出缩略图的路径和名称
-                $mbiglogo = $info['logo']['savepath'] . 'mbig_' . $info['logo']['savename'];
-                $biglogo = $info['logo']['savepath'] . 'big_' . $info['logo']['savename'];
-                $midlogo = $info['logo']['savepath'] . 'mid_' . $info['logo']['savename'];
-                $smlogo = $info['logo']['savepath'] . 'sm_' . $info['logo']['savename'];
-                $image = new \Think\Image();
-                // 打开要生成缩略图的图片
-                $image->open('./Public/Uploads/' . $logo);
-                // 生成缩略图
-                $image->thumb(700, 700)->save('./Public/Uploads/' . $mbiglogo);
-                $image->thumb(350, 350)->save('./Public/Uploads/' . $biglogo);
-                $image->thumb(130, 130)->save('./Public/Uploads/' . $midlogo);
-                $image->thumb(50, 50)->save('./Public/Uploads/' . $smlogo);
-                /**************** 把路径放到表单中 *****************/
-                $data['logo'] = $logo;
-                $data['mbig_logo'] = $mbiglogo;
-                $data['big_logo'] = $biglogo;
-                $data['mid_logo'] = $midlogo;
-                $data['sm_logo'] = $smlogo;
-            }
+        if($_FILES['logo']['error'] == 0)
+        {
+            $ret = uploadOne('logo', 'Goods', array(
+                array(700, 700),
+                array(350, 350),
+                array(130, 130),
+                array(50, 50),
+            ));
+            $data['logo'] = $ret['images'][0];
+            $data['mbig_logo'] = $ret['images'][1];
+            $data['big_logo'] = $ret['images'][2];
+            $data['mid_logo'] = $ret['images'][3];
+            $data['sm_logo'] = $ret['images'][4];
         }
         // 获取当前时间并添加到表单中这样就会插入到数据库中
         $data['addtime'] = date('Y-m-d H:i:s', time());
-        // 将通过自定义方法将HTML实体转为大小写符号，同时过滤JS脚本等XSS攻击的脚本
+        // 我们自己来过滤这个字段
         $data['goods_desc'] = removeXSS($_POST['goods_desc']);
     }
 
