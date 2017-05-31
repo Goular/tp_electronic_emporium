d295b08c35d255027112e1313f1f124d0783ba04
Goular <zhaojt_cs@126.com>
2017-05-31 09:57:22 +0800

    添加_before_update的方法

--- Application/Admin/Model/MemberPriceModel.class.php
+++ Application/Admin/Model/MemberPriceModel.class.php
@@ -12,4 +12,33 @@ class MemberPriceModel extends Model
         $where['goods_id'] = array('eq', $goodsId);
         return $this->where($where)->select();
     }
+
+    /**
+     * 在更新之前操作的内容
+     * @param $data
+     * @param $options
+     */
+    protected function _before_update(&$data, $options)
+    {
+        $id = $options['where']['id'];
+        /********************处理会员价格********************/
+        $mp = I('post.member_price');
+        $mpModel = D('member_price');
+        //先删除原来的价格
+        $mpModel->where(array(
+            'goods_id' => array('eq', $id)
+        ))->delete();
+        foreach ($mp as $key => $value) {
+            $_v = (float)$value;
+            if ($_v > 0) {
+                $mpModel->add(array(
+                    'price' => $_v,
+                    'level_id' => $key,
+                    'goods_id' => $id
+                ));
+            }
+        }
+    }
+
+
 }
