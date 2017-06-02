2568b271ae592392eb2e90a82b5ab99937fbe468
Goular <zhaojt_cs@126.com>
2017-06-03 00:21:27 +0800

    添加主分类在商品表的字段添加

--- data/2017-06-03-1.6.sql
+++ data/2017-06-03-1.6.sql
@@ -1,4 +1,5 @@
 -- 为商品表添加分类的ID
 alter TABLE p39_goods add COLUMN cat_id mediumint unsigned not null DEFAULT 0 comment '主分类ID';
+alter TABLE p39_goods add index cat_id(cat_id);
 --修改分类ID放置的位置
 alter TABLE p39_goods change cat_id  cat_id mediumint unsigned not null DEFAULT 0 comment '主分类ID' after mbig_logo;
