db3d64c454d69fe3aeff956a0aadc9e017a01c09
Goular <zhaojt_cs@126.com>
2017-06-08 14:14:49 +0800

    提价商品类型下属性表的内容

---
+++ Application/Admin/Controller/AttributeController.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+
+//商品属性控制器
+class AttributeController extends Controller
+{
+    public function add()
+    {
+        $this->display();
+    }
+
+    public function edit()
+    {
+        $this->display();
+    }
+
+    public function lst()
+    {
+        $this->display();
+    }
+}
