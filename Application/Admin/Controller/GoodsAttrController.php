258fed6d01d9c35a91dafc953b61cab2a9570640
Goular <zhaojt_cs@126.com>
2017-06-09 15:53:13 +0800

    添加修改的内容

---
+++ Application/Admin/Controller/GoodsAttrController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+
+//商品属性控制器
+class GoodsAttrController extends Controller
+{
+    //处理删除属性
+    public function ajaxDelAttr()
+    {
+        $goodsId = addslashes(I('get.goods_id'));
+        $gaid = addslashes(I('get.gaid'));
+        $gaModel = D('goods_attr');
+        $gaModel->delete($gaid);
+        // 删除相关库存量数据
+        $gnModel = D('goods_number');
+        $gnModel->where(array('goods_id' => array('EXP', "=$goodsId or AND FIND_IN_SET($gaid, attr_list)"),
+        ))->delete();
+    }
+}
