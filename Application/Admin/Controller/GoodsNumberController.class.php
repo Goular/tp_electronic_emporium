badba20a176feacad583ca0465ea9e59e7e46286
Goular <zhaojt_cs@126.com>
2017-06-09 17:35:33 +0800

    提交RBAC的五张数据表的内容

--- Application/Admin/Controller/GoodsNumberController.class.php
+++ Application/Admin/Controller/GoodsNumberController.class.php
@@ -6,7 +6,28 @@ use Think\Controller;
 //商品库存量控制器
 class GoodsNumberController extends Controller
 {
-    public function lst(){
+    public function lst()
+    {
+        //获取接收商品的ID
+        $goodsId = I('get.id');
+        //根据商品ID取出商品所有可选的属性的值
+        $gaModel = D('goods_attr');
+        $gaData = $gaModel
+            ->alias("a")
+            ->join("LEFT JOIN __ATTRIBUTE__ b ON a.attr_id=b.id")
+            ->where(array('a.goods_id' => array('eq', $goodsId)))
+            ->select();
+
+        $this->assign(array(
+            'gaData' => $gaData,
+        ));
+
+        // 设置页面信息
+        $this->assign(array(
+            '_page_title' => '库存量',
+            '_page_btn_name' => '返回列表',
+            '_page_btn_link' => U('lst'),
+        ));
 
         $this->display();
     }
---
+++ data/2017-06-09-2.0.sql
@@ -0,0 +1,50 @@
+/*********************** RBAC ***********************************/
+
+/*********************** 权限表 ***********************************/
+DROP TABLE IF EXISTS p39_privilege;
+CREATE TABLE IF NOT EXISTS p39_privilege(
+  id mediumint unsigned not null auto_increment comment 'Id',
+  pri_name varchar(30) not null DEFAULT '' comment '权限名称',
+  module_name varchar(30) not null DEFAULT '' comment '模块名称',
+  contolller_name varchar(30) not null DEFAULT '' comment '控制器名称',
+  action_name varchar(30) not null DEFAULT '' comment '方法名称',
+  parent_id mediumint unsigned not null DEFAULT 0 comment '上级权限Id',
+  PRIMARY KEY(id)
+)engine=InnoDB DEFAULT charset=utf8 comment '权限';
+
+/*********************** 角色表 ***********************************/
+DROP TABLE IF EXISTS p39_role;
+CREATE TABLE IF NOT EXISTS p39_role(
+  id mediumint unsigned not null auto_increment comment 'Id',
+  role_name varchar(30) not null DEFAULT '' comment '角色名称',
+  PRIMARY KEY(id)
+)engine=InnoDB DEFAULT charset=utf8 comment '角色';
+
+/*********************** 管理员/人员表 ***********************************/
+DROP TABLE IF EXISTS p39_admin;
+CREATE TABLE IF NOT EXISTS p39_admin(
+  id mediumint unsigned not null auto_increment comment 'Id',
+  username VARCHAR(30) not null comment '用户名',
+  password CHAR(32) not null comment '密码',
+  PRIMARY KEY(id)
+)engine=InnoDB DEFAULT charset=utf8 comment '管理员/人员';
+
+/*********************** 角色权限表 ***********************************/
+DROP TABLE IF EXISTS p39_role_pri;
+CREATE TABLE IF NOT EXISTS p39_role_pri(
+  role_id mediumint unsigned not null comment '角色Id',
+  pri_id mediumint unsigned not null comment '权限Id',
+  KEY role_id(role_id),
+  KEY pri_id(pri_id)
+)engine=InnoDB DEFAULT charset=utf8 comment '角色权限';
+
+/*********************** 人员角色表 ***********************************/
+DROP TABLE IF EXISTS p39_admin_role;
+CREATE TABLE IF NOT EXISTS p39_admin_role(
+  admin_id mediumint unsigned not null comment '管理员Id',
+  role_id mediumint unsigned not null comment '权限Id',
+  KEY admin_id(admin_id),
+  KEY role_id(role_id)
+)engine=InnoDB DEFAULT charset=utf8 comment '人员角色';
+
+INSERT INTO p39_admin(id,username,password) VALUES(1,'root','96e79218965eb72c92a549dd5a330112');
