c0fb523ec4f8ba26681bd3026d2fcdf14b16d3cc
Goular <zhaojt_cs@126.com>
2017-06-09 16:22:35 +0800

    添加修改的内容

---
+++ Application/Admin/Controller/GoodsNumberController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+
+//商品库存量控制器
+class GoodsNumberController extends Controller
+{
+    public function lst(){
+
+        $this->display();
+    }
+}
