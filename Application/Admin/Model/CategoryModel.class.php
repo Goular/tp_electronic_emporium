1eb852c6e80370ceb8c10a8240344b2f81930c13
Goular <zhaojt_cs@126.com>
2017-06-24 15:09:35 +0800

    提交内容

--- Application/Admin/Model/CategoryModel.class.php
+++ Application/Admin/Model/CategoryModel.class.php
@@ -203,42 +203,43 @@ class CategoryModel extends Model
 //        if ($floorData)
 //            return $floorData;
 //        else {
-            // 先取出推荐到楼层的顶级分类
-            //select * from php_39 where parent_id = 0 and is_floor = '是';
-            $ret = $this->where(array(
-                'parent_id' => array('eq', 0),
+        // 先取出推荐到楼层的顶级分类
+        //select * from php_39 where parent_id = 0 and is_floor = '是';
+        $ret = $this->where(array(
+            'parent_id' => array('eq', 0),
+            'is_floor' => array('eq', '是'),
+        ))->select();
+        $goodsModel = D('Admin/Goods');
+        // 循环每个楼层取出楼层中的数据
+        foreach ($ret as $k => $v) {
+            /****************** 这个楼层中的品牌数据 *********************/
+            // 先取出这个楼层下所有的商品ID
+            $goodsId = $goodsModel->getGoodsIdByCatId($v['id']);
+            if ($goodsId) {
+                // 再取出这些商品所用到的品牌
+                $ret[$k]['brand'] = $goodsModel->alias('a')
+                    ->join('LEFT JOIN __BRAND__ b ON a.brand_id=b.id')
+                    ->field('DISTINCT brand_id,b.brand_name,b.logo')
+                    ->where(array(
+                        'a.id' => array('in', $goodsId),
+                        'a.brand_id' => array('neq', 0),
+                    ))->limit(9)->select();
+            }
+            /**** 取出未推荐的二级分类并保存到这个顶级分类的subCat字段中 *************/
+            $ret[$k]['subCat'] = $this->where(array(
+                'parent_id' => array('eq', $v['id']),
+                'is_floor' => array('eq', '否'),
+            ))->select();
+            /**** 取出推荐的二级分类并保存到这个顶级分类的subCat字段中 *************/
+            $ret[$k]['recSubCat'] = $this->where(array(
+                'parent_id' => array('eq', $v['id']),
                 'is_floor' => array('eq', '是'),
             ))->select();
-            $goodsModel = D('Admin/Goods');
-            // 循环每个楼层取出楼层中的数据
-            foreach ($ret as $k => $v) {
-                /****************** 这个楼层中的品牌数据 *********************/
-                // 先取出这个楼层下所有的商品ID
-                $goodsId = $goodsModel->getGoodsIdByCatId($v['id']);
-                if($goodsId){
-                    // 再取出这些商品所用到的品牌
-                    $ret[$k]['brand'] = $goodsModel->alias('a')
-                        ->join('LEFT JOIN __BRAND__ b ON a.brand_id=b.id')
-                        ->field('DISTINCT brand_id,b.brand_name,b.logo')
-                        ->where(array(
-                            'a.id' => array('in', $goodsId),
-                            'a.brand_id' => array('neq', 0),
-                        ))->limit(9)->select();
-                }
-                /**** 取出未推荐的二级分类并保存到这个顶级分类的subCat字段中 *************/
-                $ret[$k]['subCat'] = $this->where(array(
-                    'parent_id' => array('eq', $v['id']),
-                    'is_floor' => array('eq', '否'),
-                ))->select();
-                /**** 取出推荐的二级分类并保存到这个顶级分类的subCat字段中 *************/
-                $ret[$k]['recSubCat'] = $this->where(array(
-                    'parent_id' => array('eq', $v['id']),
-                    'is_floor' => array('eq', '是'),
-                ))->select();
-                /********* 循环每个推荐的二级分类取出分类下的8件被推荐到楼层的商品 *********/
-                foreach ($ret[$k]['recSubCat'] as $k1 => &$v1) {
-                    // 取出这个分类下所有商品的ID并返回一维数组
-                    $gids = $goodsModel->getGoodsIdByCatId($v1['id']);
+            /********* 循环每个推荐的二级分类取出分类下的8件被推荐到楼层的商品 *********/
+            foreach ($ret[$k]['recSubCat'] as $k1 => &$v1) {
+                // 取出这个分类下所有商品的ID并返回一维数组
+                $gids = $goodsModel->getGoodsIdByCatId($v1['id']);
+                if ($gids) {
                     // 再根据商品ID取出商品的详细信息
                     $v1['goods'] = $goodsModel->field('id,mid_logo,goods_name,shop_price')->where(array(
                         'is_on_sale' => array('eq', '是'),
@@ -247,8 +248,9 @@ class CategoryModel extends Model
                     ))->order('sort_num ASC')->limit(8)->select();
                 }
             }
-//            S('floorData', $ret, 86400);
-            return $ret;
         }
+//            S('floorData', $ret, 86400);
+        return $ret;
+    }
 //    }
 }
