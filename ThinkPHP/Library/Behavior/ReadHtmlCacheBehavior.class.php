79a98aa0b6497290e419f3ea861cf9f4f6ebcb78
Goular <zhaojt_cs@126.com>
2017-06-26 13:34:17 +0800

    添加解决雪崩的静态化页面

--- ThinkPHP/Library/Behavior/ReadHtmlCacheBehavior.class.php
+++ ThinkPHP/Library/Behavior/ReadHtmlCacheBehavior.class.php
@@ -15,6 +15,8 @@ use Think\Storage;
 /**
  * 系统行为扩展：静态缓存读取
  */
+$fp = null;
+
 class ReadHtmlCacheBehavior
 {
     // 行为扩展的执行入口必须是run
@@ -25,6 +27,17 @@ class ReadHtmlCacheBehavior
             $cacheTime = $this->requireHtmlCache();
             if (false !== $cacheTime && $this->checkHTMLCache(HTML_FILE_NAME, $cacheTime)) {
                 //静态页面有效
+                // 读取静态页面输出
+                echo Storage::read(HTML_FILE_NAME, 'html');
+                exit();
+            }
+
+            // 如果静态页不存在或者已经过期就向后执行 --》 只让一个客户端通过这里生成静态页，其他客户端阻塞在这等待第一个客户端生成静态页
+            global $fp;
+            $fp = fopen('./read_html_cache.lock', 'r');
+            flock($fp, LOCK_EX); // 只有一个客户端可以通过其他的阻塞在这
+
+            if (false !== $cacheTime && $this->checkHTMLCache(HTML_FILE_NAME, $cacheTime)) { //静态页面有效
                 // 读取静态页面输出
                 echo Storage::read(HTML_FILE_NAME, 'html');
                 exit();
@@ -43,7 +56,7 @@ class ReadHtmlCacheBehavior
             // 'read'=>array('{id},{name}',60,'md5') 必须保证静态规则的唯一性 和 可判断性
             // 检测静态规则
             $controllerName = strtolower(CONTROLLER_NAME);
-            $actionName     = strtolower(ACTION_NAME);
+            $actionName = strtolower(ACTION_NAME);
             if (isset($htmls[$controllerName . ':' . $actionName])) {
                 $html = $htmls[$controllerName . ':' . $actionName]; // 某个控制器的操作的静态规则
             } elseif (isset($htmls[$controllerName . ':'])) {
@@ -60,32 +73,44 @@ class ReadHtmlCacheBehavior
                 // 以$_开头的系统变量
                 $callback = function ($match) {
                     switch ($match[1]) {
-                        case '_GET':$var = $_GET[$match[2]];
+                        case '_GET':
+                            $var = $_GET[$match[2]];
                             break;
-                        case '_POST':$var = $_POST[$match[2]];
+                        case '_POST':
+                            $var = $_POST[$match[2]];
                             break;
-                        case '_REQUEST':$var = $_REQUEST[$match[2]];
+                        case '_REQUEST':
+                            $var = $_REQUEST[$match[2]];
                             break;
-                        case '_SERVER':$var = $_SERVER[$match[2]];
+                        case '_SERVER':
+                            $var = $_SERVER[$match[2]];
                             break;
-                        case '_SESSION':$var = $_SESSION[$match[2]];
+                        case '_SESSION':
+                            $var = $_SESSION[$match[2]];
                             break;
-                        case '_COOKIE':$var = $_COOKIE[$match[2]];
+                        case '_COOKIE':
+                            $var = $_COOKIE[$match[2]];
                             break;
                     }
                     return (count($match) == 4) ? $match[3]($var) : $var;
                 };
                 $rule = preg_replace_callback('/{\$(_\w+)\.(\w+)(?:\|(\w+))?}/', $callback, $rule);
                 // {ID|FUN} GET变量的简写
-                $rule = preg_replace_callback('/{(\w+)\|(\w+)}/', function ($match) {return $match[2]($_GET[$match[1]]);}, $rule);
-                $rule = preg_replace_callback('/{(\w+)}/', function ($match) {return $_GET[$match[1]];}, $rule);
+                $rule = preg_replace_callback('/{(\w+)\|(\w+)}/', function ($match) {
+                    return $match[2]($_GET[$match[1]]);
+                }, $rule);
+                $rule = preg_replace_callback('/{(\w+)}/', function ($match) {
+                    return $_GET[$match[1]];
+                }, $rule);
                 // 特殊系统变量
                 $rule = str_ireplace(
                     array('{:controller}', '{:action}', '{:module}'),
                     array(CONTROLLER_NAME, ACTION_NAME, MODULE_NAME),
                     $rule);
                 // {|FUN} 单独使用函数
-                $rule = preg_replace_callback('/{|(\w+)}/', function ($match) {return $match[1]();}, $rule);
+                $rule = preg_replace_callback('/{|(\w+)}/', function ($match) {
+                    return $match[1]();
+                }, $rule);
                 $cacheTime = C('HTML_CACHE_TIME', null, 60);
                 if (is_array($html)) {
                     if (!empty($html[2])) {
@@ -110,8 +135,8 @@ class ReadHtmlCacheBehavior
      * 检查静态HTML文件是否有效
      * 如果无效需要重新更新
      * @access public
-     * @param string $cacheFile  静态文件名
-     * @param integer $cacheTime  缓存有效期
+     * @param string $cacheFile 静态文件名
+     * @param integer $cacheTime 缓存有效期
      * @return boolean
      */
     public static function checkHTMLCache($cacheFile = '', $cacheTime = '')
