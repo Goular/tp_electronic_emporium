ebf8bd83153a2404dfec335f6645219695e0f3d0
Goular <zhaojt_cs@126.com>
2017-07-03 18:02:26 +0800

    提交内容

--- Application/Admin/Model/Member.class.php
+++ Application/Admin/Model/Member.class.php
@@ -5,13 +5,53 @@ use Think\Model;
 
 class GoodsModel extends Model
 {
-    protected $insertFields = array('account','password','nickname','email');
-    protected $updateFields = array('nickname','email');
+    protected $insertFields = array('username', 'password', 'cpassword', 'chkcode', 'must_click');
+    protected $updateFields = array('id', 'username', 'password', 'cpassword');
     protected $_validate = array(
-        array('verify','require','验证码必须！'), //默认情况下用正则进行验证
-        array('name','','帐号名称已经存在！',0,'unique',1), // 在新增的时候验证name字段是否唯一
-        array('value',array(1,2,3),'值的范围不正确！',2,'in'), // 当值不为空的时候判断是否在一个范围内
-        array('repassword','password','确认密码不正确',0,'confirm'), // 验证确认密码是否和密码一致
-        array('password','checkPwd','密码格式不正确',0,'function'), // 自定义函数验证密码格式
+        array('must_click', 'require', '必须同意注册协议！', 1, 'regex', 3),
+        array('username', 'require', '用户名不能为空！', 1, 'regex', 3),
+        array('username', '1,30', '用户名的值最长不能超过 30 个字符！', 1, 'length', 3),
+        array('password', 'require', '密码不能为空！', 1, 'regex', 1),
+        array('password', '6,20', '密码的值最长不能超过 6-20 个字符！', 1, 'length', 3),
+        array('cpassword', 'password', '两次密码输入不一致！', 1, 'confirm', 3),
+        array('username', '', '用户名已经存在！', 1, 'unique', 3),
+        array('chkcode', 'require', '验证码不能为空！', 1),
+        array('chkcode', 'check_verify', '验证码不正确！', 1, 'callback'),
     );
+
+    // 为登录的表单定义一个验证规则
+    public $_login_validate = array(
+        array('username', 'require', '用户名不能为空！', 1),
+        array('password', 'require', '密码不能为空！', 1),
+        array('chkcode', 'require', '验证码不能为空！', 1),
+        array('chkcode', 'check_verify', '验证码不正确！', 1, 'callback'),
+    );
+
+    public function check_verify($code, $id = '')
+    {
+        $verify = new \Think\Verify();
+        return $verify->check($code, $id);
+    }
+
+    /**
+     * 登录操作
+     */
+    public function login()
+    {
+
+    }
+
+    /**
+     * 登出操作
+     */
+    public function logout()
+    {
+        session(null);
+    }
+
+    /**************************  钩子方法  ***************************/
+    protected function _before_insert(&$data, $options)
+    {
+        $data['password'] = md5($data['password']);
+    }
 }
---
+++ Application/Home/Controller/MemberController.class.php
@@ -0,0 +1,61 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class MemberController extends Controller
+{
+    //制作二维码
+    public function chkCode()
+    {
+        $Verify = new \Think\Verify(array(
+            'fontSize' => 30,    // 验证码字体大小
+            'length' => 2,     // 验证码位数
+            'useNoise' => false, // 关闭验证码杂点
+        ));
+        $Verify->entry();
+    }
+
+    //登录操作
+    public function login()
+    {
+        if (IS_POST) {
+            $model = D('Admin/Member');
+            if ($model->validate($model->_login_validate)->create()) {
+                if ($model->login()) {
+
+                    $this->success('登录成功');
+                    exit;
+                }
+            }
+            $this->error($model->getError());
+        }
+        // 设置页面信息
+        $this->assign(array(
+            '_page_title' => '登录',
+            '_page_keywords' => '登录',
+            '_page_description' => '登录',
+        ));
+        $this->display();
+    }
+
+    //登出操作
+    public function logout()
+    {
+        $model = D('Admin/Member');
+        $model->logout();
+        redirect('/');
+    }
+
+    //注册操作
+    public function register()
+    {
+        // 设置页面信息
+        $this->assign(array(
+            '_page_title' => '注册',
+            '_page_keywords' => '注册',
+            '_page_description' => '注册',
+        ));
+        $this->display();
+    }
+}
