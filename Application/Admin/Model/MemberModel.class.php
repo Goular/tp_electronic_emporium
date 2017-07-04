6905a132b0494fd30cdbaa0d65a13e07fe697c25
Goular <zhaojt_cs@126.com>
2017-07-04 09:37:02 +0800

    暂未添加购物车的内容到数据库

--- Application/Admin/Model/MemberModel.class.php
+++ Application/Admin/Model/MemberModel.class.php
@@ -6,7 +6,7 @@ use Think\Model;
 class MemberModel extends Model
 {
     protected $insertFields = array('username', 'password', 'cpassword', 'chkcode', 'must_click');
-    protected $updateFields = array('id', 'username', 'password', 'cpassword','chkcode');
+    protected $updateFields = array('id', 'username', 'password', 'cpassword', 'chkcode');
     protected $_validate = array(
         array('must_click', 'require', '必须同意注册协议！', 1, 'regex', 3),
         array('username', 'require', '用户名不能为空！', 1, 'regex', 3),
@@ -38,7 +38,37 @@ class MemberModel extends Model
      */
     public function login()
     {
+        //从模型中获取用户名和密码
+        $userName = $this->username;
+        $password = $this->passwork;
+        //查询用户名是否存在
+        $user = $this->field('id,username,password,jifen')
+            ->where(array(
+                'username' => array('eq', $userName)
+            ))->find();
+        if ($user) {
+            if ($user['password'] == $this->encodePassword($password)) {
+                //登录成功后存入session
+                session('m_id', $user['id']);
+                session('m_username', $user['username']);
+                //计算当前会员的级别ID 同时保存进入session
+                $mlModel = D('member_level');
+                $levelId = $mlModel->field('id')->where(array(
+                    'jifen_bottom' => array('elt', $user['jifen']),
+                    'jifen_top' => array('egt', $user['jifen']),
+                ))->find();
+                session('level_id', $levelId['id']);
 
+                //暂未添加购物车的内容到数据库
+
+            } else {
+                $msg = '密码不正确!';
+                $this->error = $msg;
+            }
+        } else {
+            $msg = '用户名不存在!';
+            $this->error = $msg;
+        }
     }
 
     /**
@@ -53,5 +83,11 @@ class MemberModel extends Model
     protected function _before_insert(&$data, $options)
     {
         $data['password'] = md5($data['password']);
+    }
+
+    /*************************** 对传进来的密码进行加密 *******************************/
+    private function encodePassword($password)
+    {
+        return md5($password);
     }
 }
