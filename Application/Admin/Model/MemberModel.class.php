22863852e18cbd5d7678c6eef462a03825965294
Goular <zhaojt_cs@126.com>
2017-07-05 21:55:21 +0800

    提交购物处理的页面

--- Application/Admin/Model/MemberModel.class.php
+++ Application/Admin/Model/MemberModel.class.php
@@ -60,7 +60,7 @@ class MemberModel extends Model
                 session('level_id', $levelId['id']);
 
                 //暂未添加购物车的内容到数据库
-                $cartModel = D('Home/Cart');
+                $cartModel = D('Admin/Cart');
                 $cartModel->moveDataToDB();
 
                 return TRUE;
