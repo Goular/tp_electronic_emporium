ca6af6f47712844624b901d86e64fc0a9955c0a9
Goular <zhaojt_cs@126.com>
2017-07-22 10:19:59 +0800

    提交修改的内容

--- data/2017-07-22-2.7.sql
+++ data/2017-07-22-2.7.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS p39_member_address;
 CREATE TABLE p39_member_address(
   id mediumint unsigned NOT NULL AUTO_INCREMENT COMMENT 'Id',
   member_id mediumint unsigned not null COMMENT '用户ID',
-  province_id mediumint unsigned not null COMMENT '省份ID',
-  city_id mediumint unsigned not null COMMENT '城市ID',
-  district_id mediumint unsigned not null COMMENT '城区ID',
+  province VARCHAR(20) not null DEFAULT '' COMMENT '省份ID',
+  city VARCHAR(20) not null DEFAULT '' COMMENT '城市ID',
+  district VARCHAR(20) not null DEFAULT '' COMMENT '城区ID',
   mobile VARCHAR(20) not null DEFAULT '' COMMENT '手机号码',
   detail VARCHAR(120) not null DEFAULT '' COMMENT '详细地址',
   is_default enum('是','否') DEFAULT '否' COMMENT '默认地址',
