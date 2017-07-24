a7e0672f1abaa4b74e8d7446daf1fa3d4dbe61c9
Goular <zhaojt_cs@126.com>
2017-07-24 16:32:28 +0800

    提交修改的内容

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -209,6 +209,21 @@ function formatUploadFiles($arrayName)
 /**
  * 从当前URL中去掉某个参数之后的URL
  */
-function makeAliPayBtn($orderId,$btnName='去支付宝支付'){
+function makeAliPayBtn($orderId, $btnName = '去支付宝支付')
+{
     return require('./alipay/alipayapi.php');
+}
+
+/**
+ * 从当前URL中去掉某个参数之后的URL
+ *
+ * @param unknown_type $param
+ */
+function filterUrl($param)
+{
+    // 先取出当前的URL地址
+    $url = $_SERVER['PHP_SELF'];
+    // 正则去掉某个参数
+    $re = "/\/$param\/[^\/]+/";
+    return preg_replace($re, '', $url);
 }
