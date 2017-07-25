6202b5d2e4bb5ee1b57eba29d947b3d5795cfc4d
Goular <zhaojt_cs@126.com>
2017-07-25 10:27:02 +0800

    提交修改的内容

--- Application/Home/Controller/SearchController.class.php
+++ Application/Home/Controller/SearchController.class.php
@@ -8,18 +8,17 @@ class SearchController extends NavController
     public function cat_search()
     {
         $cat_Id = I('get.cat_id');
-//        //取出商品和进行翻页
-//        $goodsModel = D('Admin/Goods');
-//        $data = $goodsModel->cat_search($cat_Id);
+        //取出商品和进行翻页
+        $goodsModel = D('Admin/Goods');
+        $data = $goodsModel->cat_search($cat_Id);
 
         $catModel = D('Admin/Category');
         $searchFilter = $catModel->getSearchConditionByCatId($cat_Id);
 
-
-
-
         // 设置页面信息
         $this->assign(array(
+            'page' => $data['page'],
+            'data' => $data['data'],
             'searchFilter' => $searchFilter,
             '_page_title' => '分类搜索页',
             '_page_keywords' => '分类搜索页',
