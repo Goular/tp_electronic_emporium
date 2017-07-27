66866d7cddec699fe2cf864ace3a9f79adbbc8a5
Goular <zhaojt_cs@126.com>
2017-07-27 14:18:02 +0800

    提交内容

--- Application/Admin/Model/CommentModel.php
+++ Application/Admin/Model/CommentModel.php
@@ -62,4 +62,85 @@ class CommentModel extends Model
             }
         }
     }
+
+
+    /**
+     * 取出一件商品的评论
+     */
+    public function search($goodsId, $pageSize = 5)
+    {
+// 这里因为要做AJAX翻页，所以我们需要自己做翻页不能用TP自带的。自带的不是AJAX的一点击就跳转了
+        $where['a.goods_id'] = array('eq', $goodsId);
+
+        // 取出总的记录数
+        $count = $this->alias('a')->where($where)->count();
+        // 计算总的页数
+        $pageCount = ceil($count / $pageSize);
+        // 获取当前页
+        $currentPage = max(1, (int)I('get.p', 1)); // >= 1 的整数
+        // 计算limit上的第一个参数：偏移量
+        $offset = ($currentPage - 1) * $pageSize;
+
+        // 如果是获取第一页的评论就把好评率和印象数据也取出来
+        if ($currentPage == 1) {
+            // 好评率
+            // 取出所有的分值
+            $stars = $this->field('star')->where(array(
+                'goods_id' => array('eq', $goodsId),
+            ))->select();
+            // 循环所有分值进行统计
+            $hao = $zhong = $cha = 0;
+            foreach ($stars as $k => $v) {
+                if ($v['star'] == 3)
+                    $zhong++;
+                elseif ($v['star'] > 3)
+                    $hao++;
+                else
+                    $cha++;
+            }
+            $total = $hao + $zhong + $cha;  // 总的评论数
+            $hao = round(($hao / $total) * 100, 2);
+            $zhong = round(($zhong / $total) * 100, 2);
+            $cha = round(($cha / $total) * 100, 2);
+            // 取印象
+            $yxModel = D('Yinxiang');
+            $yxData = $yxModel->where(array(
+                'goods_id' => array('eq', $goodsId),
+            ))->select();
+        }
+
+        // 取数据
+        $data = $this->alias('a')
+            ->field('a.id,a.content,a.addtime,a.star,a.click_count,b.face,b.username,COUNT(c.id) reply_count')
+            ->join('LEFT JOIN __MEMBER__ b ON a.member_id=b.id 
+		        LEFT JOIN __COMMENT_REPLY__ c ON a.id=c.comment_id')
+            ->where($where)
+            ->order('a.id DESC')
+            ->limit("$offset,$pageSize")
+            ->group('a.id')
+            ->select();
+
+        $crModel = D('comment_reply');
+        // 循环每个评论再取回复
+        foreach ($data as $k => &$v) {
+            $v['reply'] = $crModel->alias('a')
+                ->field('a.content,a.addtime,b.username,b.face')
+                ->join('LEFT JOIN __MEMBER__ b ON a.member_id=b.id')
+                ->where(array(
+                    'a.comment_id' => $v['id'],
+                ))
+                ->order('a.id ASC')
+                ->select();
+        }
+
+        return array(
+            'data' => $data,
+            'pageCount' => $pageCount,
+            'hao' => $hao,
+            'zhong' => $zhong,
+            'cha' => $cha,
+            'yxData' => $yxData,
+            'memberId' => (int)session('m_id'),
+        );
+    }
 }
