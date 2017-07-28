a04c4ab312aed6b713feb96746cf143e9dbda4f0
Goular <zhaojt_cs@126.com>
2017-07-28 13:21:05 +0800

    提交内容

---
+++ updateNewGoodsSphinx.bat
@@ -0,0 +1,2 @@
+F:\coreseek-3.2.14-win32\bin\indexer.exe -c F:\coreseek-3.2.14-win32\sphinx.conf goods_new --rotate
+F:\coreseek-3.2.14-win32\bin\indexer.exe -c F:\coreseek-3.2.14-win32\sphinx.conf --merge goods goods_new --rotate
