df9b02926b7559773658b5a5b70ae75d5eec77eb
Goular <zhaojt_cs@126.com>
2017-08-01 17:46:36 +0800

    隐藏index.php操作

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,6 +10,7 @@ return array(
     'DB_PREFIX' => 'p39_',    // 数据库表前缀
     'DB_CHARSET' => 'utf8',      // 数据库编码
     'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志,
+    'URL_MODEL' => 2,
     'SHOW_PAGE_TRACE' => true,
     'DEFAULT_FILTER' => 'trim,htmlspecialchars', // 默认参数过滤方法 用于I函数...
     'UMEDITOR_URL' => __ROOT__ . '/Public/Tools/umeditor1.2.3-utf8-php',
